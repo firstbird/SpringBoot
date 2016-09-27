742c05720af03364d745b2b6cf8c067d7344a24b
mzl0608 <mzlzjhz@163.com>
2016-09-28 07:33:20 +0800

    init spring proj

---
+++ src/main/java/com/jpaProj/CloudAppApplication.java
@@ -0,0 +1,12 @@
+package com.jpaProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAppApplication.class, args);
+	}
+}
